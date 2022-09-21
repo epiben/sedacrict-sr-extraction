6d0103b7e156e2be14aba078a6cc43fcb845f516
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2022-09-21 22:49:06 +0200

    Fix complete-form validation

--- server.R
+++ server.R
@@ -115,8 +115,12 @@ server <- function(session, input, output) {
   }
 
   ascertain_required_fields <- function(pruned_input) {
+    message(now())
+    print(pruned_input)
     exempt_idx <- names(pruned_input) == "remarks"
-    all(map_lgl(pruned_input[!exempt_idx], ~ isTRUE(nchar(.) > 0 & !is.null(.))))
+    tests <- map_lgl(pruned_input[!exempt_idx], ~ isTRUE(nchar(.) > 0 & !is.null(.)))
+    print(tests)
+    all(tests)
   }
 
   # REACTIVE DATA OBJECTS ====
